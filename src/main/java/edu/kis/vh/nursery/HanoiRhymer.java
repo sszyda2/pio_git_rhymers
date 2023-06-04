0504062d2601547a1cf5046f60bd712cc4408dc9
sszyda2 <240814@edu.p.lodz.pl>
2023-06-04 22:08:59 +0200

    17. Wygenerowanie dokumentacji wybranej klasy

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,13 +4,23 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * @return total number of rejected rhymers
+     */
     public int getTotalRejected() {
         return totalRejected;
     }
 
+    /**
+     * @return total number of rejected rhymers
+     */
     public int reportRejected() {
         return totalRejected;
     }
+
+    /**
+     * @param in line 25 increases if a number is rejected
+     */
 
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
